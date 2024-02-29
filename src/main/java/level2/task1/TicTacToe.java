4f86a8908085bfb29f071deb4916e9e5241bd7e6
prernanimje <prernanimje5@gmail.com>
2024-02-29 11:38:55 +0530

    level 2 task 3

--- src/main/java/level2/task1/TicTacToe.java
+++ src/main/java/level2/task1/TicTacToe.java
@@ -1,6 +1,4 @@
-package level2.task1.tictactoe;
-
-import com.sun.source.tree.BreakTree;
+package level2.task1;
 
 import java.util.Scanner;
 
@@ -127,4 +125,58 @@ public class TicTacToe {
         }
 
     }
+//    char[][] board;
+//    public TicTacToe(){
+//        board=new char[3][3];
+//        initBoard();
+//    }
+//    void initBoard(){
+//        for (int i=0;i< board.length;i++){
+//            for (int j=0;j< board[i].length;j++){
+//                board[i][j]=' ';
+//            }
+//        }
+//    }
+//    void dispBoard(){
+//        System.out.println("--------------");
+//        for (int i=0;i< board.length;i++){
+//            System.out.print(" | ");
+//            for (int j=0;j< board[i].length;j++){
+//                System.out.print(board[i][j] +" | ");
+//            }
+//            System.out.println();
+//            System.out.println("--------------");
+//        }
+//    }
+//
+//    void placeMark(int row,int column,char mark){
+//        if(row >=0 && row <=2 && column >=0 && column <=2){
+//            board[row][column]=mark;
+//        }
+//        else {
+//            System.out.println("Invalid Position");
+//        }
+//    }
+//    Boolean checkColWin(){
+//        for (int j=0;j<=2;j++){
+//            if (board[0][j]!=' ' && board[0][j]== board[1][j] && board[1][j]== board[2][j]){
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+//    Boolean checkRowsWin(){
+//        for (int i=0;i<=2;i++){
+//            if (board[i][0]!=' ' && board[i][0]==board[i][1] && board[i][1]==board[i][2]){
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+//    Boolean checkDiagWin(){
+//        if (board[0][0]!=' ' && board[0][0]==board[1][1] && board[1][1]==board[2][2] || board[0][2]!=' '&& board[0][2]==board[1][1] && board[1][1]==board[2][1]){
+//            return true;
+//        }
+//        return false;
+//    }
 
